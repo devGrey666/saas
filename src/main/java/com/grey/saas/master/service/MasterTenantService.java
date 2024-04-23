8eeadb6371e0d487c5aacfa729761b762eee42e8
dev <dev-pc>
2024-04-23 10:51:12 +0500

    add service

---
+++ src/main/java/com/grey/saas/master/service/MasterTenantService.java
@@ -0,0 +1,10 @@
+package com.grey.saas.master.service;
+
+import com.grey.saas.master.model.MasterTenantEntity;
+
+import java.util.Optional;
+
+public interface MasterTenantService {
+
+    Optional<MasterTenantEntity> findByTenantId(String tenantId);
+}
---
+++ src/main/java/com/grey/saas/master/service/MasterTenantServiceImpl.java
@@ -0,0 +1,21 @@
+package com.grey.saas.master.service;
+
+import com.grey.saas.master.model.MasterTenantEntity;
+import com.grey.saas.master.repository.MasterTenantRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@AllArgsConstructor
+@Component
+public class MasterTenantServiceImpl implements MasterTenantService {
+
+
+    MasterTenantRepository repository;
+
+    @Override
+    public Optional<MasterTenantEntity> findByTenantId(String tenantId) {
+        return repository.findByTenantId(tenantId);
+    }
+}
