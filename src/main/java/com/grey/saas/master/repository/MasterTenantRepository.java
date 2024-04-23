10113bb90f60e5b659b8ad8548dd9ff75c276922
dev <dev-pc>
2024-04-23 10:51:03 +0500

    add repo

---
+++ src/main/java/com/grey/saas/master/repository/MasterTenantRepository.java
@@ -0,0 +1,12 @@
+package com.grey.saas.master.repository;
+
+import com.grey.saas.master.model.MasterTenantEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MasterTenantRepository extends JpaRepository<MasterTenantEntity, Long> {
+    Optional<MasterTenantEntity> findByTenantId(String tenantId);
+}
